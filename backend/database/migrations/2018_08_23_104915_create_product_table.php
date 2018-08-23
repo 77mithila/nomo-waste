e6f346003177dd4fbf57cbfaa4839c18a20a9458
jonathan <jonathan@jjok.co.uk>
2018-08-23 16:16:27 +0100

    Add product migration

---
+++ backend/database/migrations/2018_08_23_104915_create_product_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+final class CreateProductTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table)
+        {
+//            $table->increments('id');
+            $table->integer('barcode');
+            $table->integer('category_id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
