f32de3a9645676ab3876e6c4abd13f049ddec20c
jonathan <jonathan@jjok.co.uk>
2018-08-24 13:21:26 +0100

    Add seeder for dummy products

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +12,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // $this->call(UsersTableSeeder::class);
+        for($n = 0; $n < 100; $n++) {
+            $id = DB::table('product')->insertGetId([
+                'barcode' => random_int(11111111, 99999999),
+                'name' => str_random(10),
+                'category_id' => random_int(1, 12),
+            ]);
 
-
-
+            DB::table('batch')->insert([
+                'quantity' => random_int(10, 50),
+                'expiration_date' => sprintf(
+                    '%u-%u-%u',
+                    2018,
+                    random_int(9, 12),
+                    random_int(1, 30)
+                ),
+                'product_id' => $id,
+                'retail_price' => random_int(50, 500),
+            ]);
+        }
     }
 }
