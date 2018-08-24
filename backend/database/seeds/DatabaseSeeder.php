48e11936006d8dedcff8465a4de0ac18ca778caf
Mithila Wanasinghe <mithila_77@yahoo.com>
2018-08-24 23:00:35 +0200

    seed database with store ids and multiple batch records per product

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -16,13 +16,30 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UsersTableSeeder::class);
         for($n = 0; $n < 100; $n++) {
+            $storeId = random_int(1,3);
+
             $id = DB::table('product')->insertGetId([
+                'store_id' => $storeId,
                 'barcode' => random_int(11111111, 99999999),
-                'name' => $faker->text(10),
-                'category_id' => random_int(1, 12),
+                'name' => $faker->word(),
+                'category_id' => random_int(1, 8),
             ]);
 
             DB::table('batch')->insert([
+                'store_id' => $storeId,
+                'quantity' => random_int(10, 50),
+                'expiration_date' => sprintf(
+                    '%u-%u-%u',
+                    2018,
+                    random_int(8, 12),
+                    random_int(1, 30)
+                ),
+                'product_id' => $id,
+                'retail_price' => random_int(50, 500),
+            ]);
+
+            DB::table('batch')->insert([
+                'store_id' => $storeId,
                 'quantity' => random_int(10, 50),
                 'expiration_date' => sprintf(
                     '%u-%u-%u',
@@ -37,21 +54,21 @@ class DatabaseSeeder extends Seeder
 
         DB::table('manager')->insert(
             [
-                'store_id' => 12,
+                'store_id' => 1,
                 'name' => 'Pirate Captain',
                 'username' => 'pirate',
                 'password' => 'arr'
             ]);
         DB::table('manager')->insert(
             [
-                'store_id' => 34,
+                'store_id' => 2,
                 'name' => 'Viking Hero',
                 'username' => 'viking',
                 'password' => 'grr'
             ]);
         DB::table('manager')->insert(
             [
-                'store_id' => 56,
+                'store_id' => 3,
                 'name' => 'Chief Sailor',
                 'username' => 'sailor',
                 'password' => 'brr'
