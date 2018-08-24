7a80a371100e5f4deb5953ac961e012c3d1e95bd
Mithila Wanasinghe <mithila_77@yahoo.com>
2018-08-24 16:14:04 +0200

    deduct inventory integrated with product list

--- backend/resources/views/product/deduct-inventory-form.blade.php
+++ backend/resources/views/product/deduct-inventory-form.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 @include('partials.breadcrumb', [
-'title' => 'Deduct Inventory from System',
+'title' => 'Update Inventory Quantities',
 'linkOne' => true,
 'linkOneName' => 'Inventory',
 'linkOneUrl' => '#',
@@ -15,14 +15,22 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="ibox-title">
-            <h5>Input deduct information</h5>
+            <h4>Update Inventory
+            @if($product)
+                <small>{{$product->name}}</small>
+            @endif
+            </h4>
         </div>
         <div class="ibox-content">
             <form action="/deduct-inventory" method="post" class="form-horizontal">
                 <div class="form-group">
                     <label class="col-sm-2 control-label">Barcode</label>
                     <div class="col-sm-10">
-                        <input name="barcode" type="number" required="required">
+                        @if($product)
+                            <input name="barcode" type="number" required="required" value="{{$product->barcode}}">
+                            @else
+                            <input name="barcode" type="number" required="required">
+                            @endif
                     </div>
                 </div>
                 <div class="hr-line-dashed"></div>
@@ -37,7 +45,7 @@
                 <div class="form-group">
                     <div class="col-sm-4 col-sm-offset-2">
                         <a class="btn btn-warning" href="/">Cancel</a>
-                        <button class="btn btn-primary" type="submit">Done</button>
+                        <button class="btn btn-primary" type="submit">Update</button>
                     </div>
                 </div>
             </form>
