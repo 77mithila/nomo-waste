00af2f0fca4297bdc5f4ceeaed0e0fa72a60ed73
Mithila Wanasinghe <mithila_77@yahoo.com>
2018-08-24 13:26:49 +0200

    styling the product list started

--- backend/resources/views/product/list.blade.php
+++ backend/resources/views/product/list.blade.php
@@ -1,3 +1,17 @@
+@extends('layout')
+
+@section('content')
+
+    @include('partials.breadcrumb', [
+                                        'title' => 'List of Existing Products',
+                                        'linkOne' => true,
+                                        'linkOneName' => 'Product',
+                                        'linkOneUrl' => '#',
+                                        'linkTwo' => true,
+                                        'linkTwoName' => 'Products List',
+                                        'linkTwoUrl' => '#',
+                                    ])
+
 
 @foreach ($products as $product)
     <div>
@@ -5,3 +19,5 @@
         <a href="/add-batch-form?product_id={{ $product->id }}">Add batch</a>
     </div>
 @endforeach
+
+@endsection
