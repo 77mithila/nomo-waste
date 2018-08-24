8c9a359319c7434b76dee9242b6219fb1e6c803b
Vy Le <hule@itu.dk>
2018-08-24 10:08:32 +0200

    fix category id

--- backend/resources/views/product/product-create-form.blade.php
+++ backend/resources/views/product/product-create-form.blade.php
@@ -25,16 +25,16 @@
         <option value="0">Select</option>
         <option value="1">Beverages</option>
         <option value="2">Bread / Bakery</option>
-        <option value="2">Canned / Jarred</option>
-        <option value="2">Dairy</option>
-        <option value="2">Dray / Baking</option>
-        <option value="2">Frozen</option>
-        <option value="2">Fresh Meat</option>
-        <option value="2">Produce</option>
-        <option value="2">Cleaners</option>
-        <option value="2">Paper Goods</option>
-        <option value="2">Personal Care</option>
-        <option value="2">Other</option>
+        <option value="3">Canned / Jarred</option>
+        <option value="4">Dairy</option>
+        <option value="5">Dray / Baking</option>
+        <option value="6">Frozen</option>
+        <option value="7">Fresh Meat</option>
+        <option value="8">Produce</option>
+        <option value="9">Cleaners</option>
+        <option value="10">Paper Goods</option>
+        <option value="11">Personal Care</option>
+        <option value="12">Other</option>
     </select>
     <br>
     @csrf
