66241ee458f900b686447917797710ef141924fc
Mithila Wanasinghe <mithila_77@yahoo.com>
2018-08-24 16:16:39 +0200

    more styling

--- backend/resources/views/expiring-inventory.blade.php
+++ backend/resources/views/expiring-inventory.blade.php
@@ -11,28 +11,37 @@
                                         'linkTwoName' => 'Expiring Items',
                                         'linkTwoUrl' => '#',
                                     ])
-
-    <table class="table table-bordered table-striped">
-        <thead>
-        <tr>
-            <th>Product Name</th>
-            <th>Barcode</th>
-            <th>Category</th>
-            <th>Batch Qty</th>
-            <th>Expire Date</th>
-            <th>Actions</th>
-        </tr>
-        </thead>
-        @foreach ($inventory as $batch)
-            <tr>
-                <td>{{ $batch->product->name }}</td>
-                <td>{{ $batch->barcode() }}</td>
-                <td>{{ $batch->product->categoryName() }}</td>
-                <td>{{ $batch->quantity  }}</td>
-                <td>{{ $batch->expirationDate()->format('d-m-Y') }}</td>
-                <td><a class="btn btn-sm btn-success" href="/create-offer-form?batch_id={{ $batch->id }}">Create offer</a></td>
-            </tr>
-        @endforeach
-    </table>
-
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-content">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>Product Name</th>
+                                <th>Barcode</th>
+                                <th>Category</th>
+                                <th>Batch Qty</th>
+                                <th>Expire Date</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            @foreach ($inventory as $batch)
+                                <tr>
+                                    <td>{{ $batch->product->name }}</td>
+                                    <td>{{ $batch->barcode() }}</td>
+                                    <td>{{ $batch->product->categoryName() }}</td>
+                                    <td>{{ $batch->quantity  }}</td>
+                                    <td>{{ $batch->expirationDate()->format('d-m-Y') }}</td>
+                                    <td><a class="btn btn-sm btn-success"
+                                           href="/create-offer-form?batch_id={{ $batch->id }}">Create offer</a></td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- backend/resources/views/product/list.blade.php
+++ backend/resources/views/product/list.blade.php
@@ -16,12 +16,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="ibox float-e-margins">
-                    <div class="ibox-title">
-                        <h5>Input new product information</h5>
-                    </div>
                     <div class="ibox-content">
-
-
                         <table class="table table-bordered table-striped">
                             <thead>
                             <tr>
