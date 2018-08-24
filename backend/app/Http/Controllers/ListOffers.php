879c085ee503420e57cc24619ff1f641d4c52bee
Mithila Wanasinghe <mithila_77@yahoo.com>
2018-08-24 18:20:01 +0200

    sql beautified

--- backend/app/Http/Controllers/ListOffers.php
+++ backend/app/Http/Controllers/ListOffers.php
@@ -10,8 +10,12 @@ class ListOffers extends Controller
 {
     public function __invoke()
     {
-        $result = DB::select(DB::raw("SELECT retail_price, discount_percentage, end_date, batch_id, barcode, product.name FROM offer,batch,product WHERE batch_id = batch.id AND product_id = product.id"));
+        $offers = DB::select(
+            DB::raw(
+                "SELECT retail_price, discount_percentage, end_date, batch_id, barcode, product.name
+                 FROM offer ,batch ,product
+                 WHERE batch_id = batch.id AND product_id = product.id"));
 
-        return view('offer.list-offer',array('offers' => $result));
+        return view('offer.list-offer',compact('offers'));
     }
 }
